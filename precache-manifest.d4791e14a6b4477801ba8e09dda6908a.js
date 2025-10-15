a9009e683da006da0a9063c0a09a08f4564b379d
Nicola <nicola.macoir@rightcrowd.com>
2025-10-15 21:03:31 +0200

    initial commit

---
+++ precache-manifest.d4791e14a6b4477801ba8e09dda6908a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "372ebf752da54498fb53",
+    "url": "/SlimsteMensApp/static/css/main.7dc58458.chunk.css"
+  },
+  {
+    "revision": "372ebf752da54498fb53",
+    "url": "/SlimsteMensApp/static/js/main.372ebf75.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/SlimsteMensApp/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "1d4a0da8683f8e4c88b7",
+    "url": "/SlimsteMensApp/static/js/runtime~main.1d4a0da8.js"
+  },
+  {
+    "revision": "90af810e24b55e15664297bb857fca35",
+    "url": "/SlimsteMensApp/index.html"
+  }
+];
