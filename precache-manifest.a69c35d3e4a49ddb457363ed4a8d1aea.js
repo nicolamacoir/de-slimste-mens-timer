e2a1d11d73cdeffab60bfc2cbde01dbf06e83b92
Nicola <nicola.macoir@rightcrowd.com>
2025-10-15 21:11:08 +0200

    Updates

---
+++ precache-manifest.a69c35d3e4a49ddb457363ed4a8d1aea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "572c10376f96f44c69f9",
+    "url": "/de-slimste-mens-timer/static/css/main.7dc58458.chunk.css"
+  },
+  {
+    "revision": "572c10376f96f44c69f9",
+    "url": "/de-slimste-mens-timer/static/js/main.572c1037.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/de-slimste-mens-timer/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "bd0c01f591dba9544388",
+    "url": "/de-slimste-mens-timer/static/js/runtime~main.bd0c01f5.js"
+  },
+  {
+    "revision": "077e4682da446b27bc630dffd13e7464",
+    "url": "/de-slimste-mens-timer/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/de-slimste-mens-timer/precache-manifest.a69c35d3e4a49ddb457363ed4a8d1aea.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/de-slimste-mens-timer/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
